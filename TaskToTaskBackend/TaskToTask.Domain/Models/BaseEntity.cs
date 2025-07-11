2ea728f95e21e80894c569b394ae9d3d4468a7bd
smorodya74 <positivepel@gmail.com>
2025-07-11 19:20:14 +0500

    Backend: созданы базовые абстрактные сущности
    - Добавлен пакет MediatR

---
+++ TaskToTaskBackend/TaskToTask.Domain/Models/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskToTask.Domain.Models
+{
+    public abstract class BaseEntity
+    {
+        protected BaseEntity(Guid id)
+        {
+            this.Id = id;
+        }
+
+        /// <summary>
+        /// Уникальный идентификатор сущности
+        /// </summary>
+        public Guid Id { get; }
+    }
+}
---
+++ TaskToTaskBackend/TaskToTask.Domain/Models/BaseEntityWithDates.cs
@@ -0,0 +1,30 @@
+﻿namespace TaskToTask.Domain.Models
+{
+    public abstract class BaseEntityWithDates : BaseEntity
+    {
+        protected BaseEntityWithDates(Guid id, DateTime createdAt, DateTime updatedAt) : base(id)
+        {
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+
+        }
+
+        /// <summary>
+        /// Дата и время создания [dd.MM.yyyy HH:mm]
+        /// </summary>
+        public DateTime? CreatedAt { get; }
+        
+        /// <summary>
+        /// Дата и время редактирования [dd.MM.yyyy HH:mm]
+        /// </summary>
+        public DateTime? UpdatedAt { get; private set; }
+
+        /// <summary>
+        /// Обновить дату изменения
+        /// </summary>
+        public virtual void SetUpdateAt(DateTime updatedAt)
+        {
+            UpdatedAt = updatedAt;
+        }
+    }
+}
